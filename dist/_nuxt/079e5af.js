621529c4bd91c10510cd01f325623fc18b4e4cac
stephanSAkhtar <stephan.a@eztechmedia.com>
2023-12-13 18:45:23 +0500

    build

---
+++ dist/_nuxt/079e5af.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{1284:function(n,e,t){"use strict";t.r(e);var o={name:"IndexPage",components:{theBody:t(1283).default}},l=t(65),component=Object(l.a)(o,(function(){var n=this._self._c;return n("div",[n("the-body")],1)}),[],!1,null,null,null);e.default=component.exports}}]);
