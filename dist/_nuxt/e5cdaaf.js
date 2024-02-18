312d0de394051fc3b5f8f3e13d604c19b7bda7d8
Shahzeb Akhtar <130976080+Shahzebakhtar06@users.noreply.github.com>
2024-02-18 09:16:03 +0500

    Add files via upload
    18-2-2024 build

---
+++ dist/_nuxt/e5cdaaf.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{1293:function(n,e,t){"use strict";t.r(e);var o={name:"IndexPage",components:{theBody:t(1292).default}},l=t(51),component=Object(l.a)(o,(function(){var n=this._self._c;return n("div",[n("the-body")],1)}),[],!1,null,null,null);e.default=component.exports}}]);
